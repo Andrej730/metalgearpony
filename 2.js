6e531fab136f794185258bcc801e65ffd13123e0
Andrej730 <azhilenkov@gmail.com>
2014-11-01 14:43:42 +0300

    14:42, 01/11/14
    Blinking cursor, black background, input textfield, some changes in
    text.

--- 2.js
+++ 2.js
@@ -1,36 +1,36 @@
 function Game() {
 	var write = function(string) {
 		$('#terminal').append('<p>'+string+'</p>');
-		$('#cursor').appendTo($('#terminal').children(':last-child'));
-
 		if ($('#terminal').children().length > 10) {
 			$('#terminal').children(':first').remove();
 		}
+		
+
 	}
 
 	this.interpret = function(string) {
-		commands = string.toLowerCase().split(' ')
+		commands = string.split(' ')
 		switch(commands[0]) {
 			case 'go':
 				switch(commands[1]) {
 					case 'west':
-						write('Going west.');
+						write('going west');
 						break;
 					case 'north':
-						write('Going north.');
+						write('going north');
 						break;
 					case 'east':
-						write('Going east.');
+						write('going east');
 						break;
 					case 'south':
-						write('Going south.');
+						write('going south');
 						break;
 					default:
-						write('There is no such thing as ' + commands[1] + '.');
+						write('theres no such thing as ' + commands[1]);
 				}
 				break;
 			default:
-				write('I dont know how to ' + commands[0] + '.')
+				write('You say bullshit, man.')
 		}
 	}
 }
