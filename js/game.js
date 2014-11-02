bcda50a3dc23852c4ff98e12ac77bf870e6238f3
Andrej730 <azhilenkov@gmail.com>
2014-11-02 16:49:53 +0300

    16:47
    White color for links, added log function, added new type of objects -
    World (map, spawn etc) and Thing.

--- js/game.js
+++ js/game.js
@@ -1,8 +1,7 @@
-function Game() {
-	this.player = new Player(0,0);
-	//this.world = new World(this);
+log = "";
 
-	var write = function(string, color) {
+function Game() {
+	this.write = function(string, color) {
 		//green - 1F9822, red - C71F0B, yellow - D4C518, blue - 0C61B8
 		color = typeof color !== 'undefined' ? color : "#FFF";
 		$('#terminal').append('<p style="color:' + color + ';">'+string+'</p>');
@@ -14,44 +13,82 @@ function Game() {
 	}
 
 	this.interpret = function(string) {
-		commands = string.toLowerCase().split(' ')
-		write('>'+string.toLowerCase(), '#1F9822')
-		switch(commands[0]) {
-			case 'go':
-				switch(commands[1]) {
-					case 'west':
-						write('Going west.');
-						this.player.x -= 1;
-						break;
-					case 'north':
-						write('Going north.');
-						this.player.y += 1;
-						break;
-					case 'east':
-						write('Going east.');
-						this.player.x += 1;
-						break;
-					case 'south':
-						write('Going south.');
-						this.player.y -= 1;
-						break;
-					default:
-						write('There is no such thing as ' + commands[1] + '.');
-				}
-				$('#x').text(this.player.x);
-				$('#y').text(this.player.y);
-				break;
-			default:
-				write('I dont know how to ' + commands[0] + '.')
+	commands = string.toLowerCase().split(' ')
+	this.write('>'+string.toLowerCase(), '#1F9822')
+	switch(commands[0]) {
+		case 'go':
+			switch(commands[1]) {
+				case 'west':
+					this.write('Going west.');
+					this.player.x -= 1;
+					break;
+				case 'north':
+					this.write('Going north.');
+					this.player.y += 1;
+					break;
+				case 'east':
+					this.write('Going east.');
+					this.player.x += 1;
+					break;
+				case 'south':
+					this.write('Going south.');
+					this.player.y -= 1;
+					break;
+				default:
+					this.write('There is no such thing as ' + commands[1] + '.');
+			}
+			$('#x').text(this.player.x);
+			$('#y').text(this.player.y);
+			break;
+		default:
+			this.write('I dont know how to ' + commands[0] + '.')
 		}
 	}
+
+	this.log = function(arg) {
+		this.write(arg);
+	}
+
+	this.world = new World(this);
+	this.player = new Player(this, 0, 0);
 }
 
-function Player(game, x, y) {
+function Player(game, x, y){
 	this.x = x;
 	this.y = y;
+}
 
-	this.getInfo = function() {
-		alert('i am exist');
+function Thing(world, x, y, name) {
+	this.x = x;
+	this.y = y;
+	this.name = name;
+}
+
+function World(game) {
+	this.map = [[0, 0, 1, 0, 0, 0, 0, 0], 
+				[0, 0, 0, 0, 0, 0, 0, 0], 
+				[0, 0, 0, 0, 0, 0, 0, 0], 
+				[0, 0, 0, 0, 0, 0, 0, 0], 
+				[0, 0, 0, 0, 0, 0, 0, 0], 
+				[0, 0, 0, 0, 0, 0, 0, 0],
+				[0, 0, 0, 0, 0, 0, 0, 0]];
+
+	this.things = new Array();
+	
+	this.showMap = function() {
+		this.map.forEach(function(arg) {
+			game.write(arg);
+		});
 	}
+
+	this.addThing = function(x, y, name) {
+		this.things[name] = new Thing(this, x, y, name);
+		game.log(name+" spawned at " + x+"; "+y);
+		this.map[y][x] = 2;
+		
+	}
+
+	this.addThing(5, 0, "Banana");
+	this.showMap();
+	game.log("Map initiated");
 }
